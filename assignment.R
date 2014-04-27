01c82360cc3f711ec55e008e412a44c4b5f22d4b
ASMSKANNER <romanov1553@yandex.ru>
2014-04-27 17:52:18 +0400

    Create assignment.R

---
+++ assignment.R
@@ -0,0 +1,30 @@
+#This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+#This function computes the inverse of the special "matrix"
+#returned by  makeCacheMatrix  above.
+#If the inverse has already been calculated (and the matrix has not changed),
+#then  cacheSolve  should retrieve the inverse from the cache.
+cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) { #if the mean has already been calculated
+    message("getting cached data")
+    return(m) #returned by  makeCacheMatrix  above
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setsolve(m)
+  m
+}
